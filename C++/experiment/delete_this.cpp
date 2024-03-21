e6724aae54557e5e1c88cc20b6d5b77802ef145b
Soham-KT <115338084+Soham-KT@users.noreply.github.com>
2024-03-21 17:33:06 +0530

    leetcode practice

--- C++/experiment/delete_this.cpp
+++ C++/experiment/delete_this.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+class Solution {
+public:
+    int getPivot(vector<int>& nums) {
+        int s = 0;
+        int e = nums.size() - 1;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] > nums[e]) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+        return s;
+    }
 
-    return 0;
-}
+    int binarySearch(vector<int>& nums, int s, int e, int key) {
+        int start = s;
+        int end = e;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == key) {
+                return mid;
+            } else if (key > nums[mid]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int pivot = getPivot(nums);
+        if (target >= nums[pivot] && target <= nums[nums.size() - 1]) {
+            return binarySearch(nums, pivot, nums.size() - 1, target);
+        } else {
+            return binarySearch(nums, 0, pivot - 1, target);
+        }
+    }
+};
